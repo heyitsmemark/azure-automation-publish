3601c67f95fa8bf26f1fa4b91c9a26726fa78ccf
Mark Elliott <Mark.Elliott@globalknowledge.net>
2023-01-26 18:24:57 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,41 +2,41 @@
 
 set -e
 
-if [ -z "$INPUT_ClientId" ]; then
+if [ -z "$INPUT_CLIENTID" ]; then
   echo "ClientId is not set."
   exit 1
 fi
 
-if [ ! -z "$INPUT_ClientSecret" ]; then
+if [ ! -z "$INPUT_CLIENTSECRET" ]; then
   echo "ClientId is not set."
   exit 1
 fi
 
-if [ ! -z "$INPUT_TenantId" ]; then
+if [ ! -z "$INPUT_TENANTID" ]; then
   echo "TenantId is not set."
   exit 1
 fi
 
-if [ ! -z "$INPUT_AutomationAccountName" ]; then
+if [ ! -z "$INPUT_AUTOMATIONACCOUNTNAME" ]; then
   echo "AutomationAccountName is not set."
   exit 1
 fi
 
-if [ ! -z "$INPUT_AutomationAccountResourceGroup" ]; then
+if [ ! -z "$INPUT_AUTOMATIONACCOUNTRESOURCEGROUP" ]; then
   echo "AutomationAccountResourceGroup is not set."
   exit 1
 fi
 
 
-az login --service-principal -u $INPUT_ClientId -p $INPUT_ClientSecret --tenant $INPUT_TenantId
+az login --service-principal -u $INPUT_CLIENTID -p $INPUT_CLIENTSECRET --tenant $INPUT_TENANTID
 
 for fullfile in ./*.ps1; do
     basefile=$(basename -- "$fullfile")
     filename="${basefile%.*}"
 
-    az automation runbook create  -g automation --automation-account-name aa-plop --runbook-name ${filename} --type PowerShell
-    az automation runbook replace-content --content @./${basefile} --automation-account-name aa-plop -g automation --name ${filename}
-    az automation runbook publish --automation-account-name aa-plop -g automation --name ${filename}
+    az automation runbook create  -g ${INPUT_AUTOMATIONACCOUNTRESOURCEGROUP} --automation-account-name ${INPUT_AUTOMATIONACCOUNTNAME} --runbook-name ${filename} --type PowerShell
+    az automation runbook replace-content --content @./${basefile} --automation-account-name ${INPUT_AUTOMATIONACCOUNTNAME} -g ${INPUT_AUTOMATIONACCOUNTRESOURCEGROUP} --name ${filename}
+    az automation runbook publish --automation-account-name ${INPUT_AUTOMATIONACCOUNTNAME} -g ${INPUT_AUTOMATIONACCOUNTRESOURCEGROUP} --name ${filename}
 done
 
 az logout
